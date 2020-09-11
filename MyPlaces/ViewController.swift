83e94d66dd04cb427fe6e170dcd42f044680dc1a
Alexandr Badmaev <a.badmaev@kingbird.ru>
2020-09-11 12:13:20 +0300

    Initial

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -9,12 +9,26 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let restaurantNames = ["Балкан Гриль", "Бочка", "Вкусные истории", "Дастархан", "Индокитай", "Классик", "Шок", "Bonsai", "Burger Heroes", "Kitchen", "Love&Life", "Morris Pub", "Sherlock Holmes", "Speak Easy", "X.O"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurantNames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else { return UITableViewCell() }
+        let restaurant = restaurantNames[indexPath.row]
+        cell.textLabel?.text = restaurant
+        cell.imageView?.image = UIImage(named: restaurant)
+        return cell
+    }
 }
 
